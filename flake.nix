7890a451af25cdab11234c9da34d5d3ffe1546d0
mudrii <mudreac@gmail.com>
2021-06-04 21:30:16 +0800

    add unstable to flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { home-manager, nixpkgs, ... }: {
+  outputs = { home-manager, nixpkgs, nixpkgs-unstable, ... }: {
     nixosConfigurations = {
       nixtst = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
