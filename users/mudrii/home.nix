e73bdbc383752b15262877cba75a6cda93321dce
mudrii <mudreac@gmail.com>
2021-06-05 18:31:31 +0800

    add apps to home manager

--- users/mudrii/home.nix
+++ users/mudrii/home.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
-
+/*
+let
+  unstable = import inputs.nixpkgs-unstable {
+    config = nixpkgs.config;
+    localSystem = "x86_64-linux";
+  };
+in
+*/
 {
 
   programs.home-manager.enable = true;
@@ -7,51 +14,24 @@
   home = {
     username = "mudrii";
     homeDirectory = "/home/mudrii";
-/*
+
     packages = with pkgs; [
-      sshfs
-      tmux
-      google-cloud-sdk-gce
-      pulumi-bin
-      gitAndTools.gitFull
-      gitAndTools.git-hub
-      gitAndTools.gh
-      git-crypt
-      git-lfs
-      kubernetes
-      kubernetes-helm
-      kubeseal
-      helmfile
-      helmsman
-      kind
-      kube3d
-      argo
-      argocd
-      kustomize
-      k9s
-      kubectx
-      nodejs
-      python38Full
-      python38Packages.poetry
-      python38Packages.pip
-      python38Packages.powerline
-      python38Packages.pygments
-      python38Packages.pygments-markdown-lexer
-      python38Packages.xstatic-pygments
-      python38Packages.pylint
-      python38Packages.numpy
-      python38Packages.pynvim
-      aspell
-      aspellDicts.en
-      asciinema
-      highlight
-      jq
-      bat
-      ripgrep-all
-      tldr
-      procs
-      fd
-      nodejs
+       tmux
+       sshfs
+       #unstable.asciinema
+       asciinema
+       highlight
+       nodejs
+       aspell
+       aspellDicts.en
+       bat
+       ripgrep-all
+       tldr
+       procs
+       fd
+       gitAndTools.gh
+       git-crypt
+       git-lfs
   ];
 */
   stateVersion = "21.05";
