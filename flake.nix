410df684bd563632a0125bda6dacadd1dad8e5a0
mudrii <mudreac@gmail.com>
2021-06-06 16:48:29 +0800

    remove home-manager

--- flake.nix
+++ flake.nix
@@ -4,24 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { home-manager, nixpkgs, nixpkgs-unstable, ... }: {
+  outputs = { nixpkgs, nixpkgs-unstable, ... }: {
     nixosConfigurations = {
       nixtst = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-/*
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mudrii = import ./users/mudrii/home.nix;
-            }
-*/
         ];
       };
     };
