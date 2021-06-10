0ea5cf7182d74c983fcab521ea61043141a561f3
mudrii <mudreac@gmail.com>
2021-06-10 10:10:52 +0800

    cleanup

--- packages.nix
+++ packages.nix
@@ -19,16 +19,16 @@
       kustomize
       k9s
       kubectx
-      jq
+#      jq
       binutils
       gnutls
       wget
       curl
-      htop
+#      htop
       bind
       mkpasswd
-      trash-cli
-      exa
+#      trash-cli
+#      exa
       direnv
       nix-direnv
     ];
@@ -40,17 +40,17 @@
       egrep = "egrep --color=auto";
       fgrep = "fgrep --color=auto";
       grep = "grep --color=auto";
-      la = "exa -alg --group-directories-first -s=type --icons";
+#      la = "exa -alg --group-directories-first -s=type --icons";
       lless = "set -gx LESSOPEN '|pygmentize -f terminal256 -g -P style=monokai %s' && set -gx LESS '-R' && less -m -g -i -J -u -Q";
-      ll = "exa -la";
-      ls = "exa";
+#      ll = "exa -la";
+#      ls = "exa";
       mv = "mv -i";
       ping = "ping -c3";
       ps = "ps -ef";
-      rm = "trash-put";
-      unrm = "trash-restore";
-      rmcl = "trash-empty";
-      rml = "trash-list";
+#      rm = "trash-put";
+#      unrm = "trash-restore";
+#      rmcl = "trash-empty";
+#      rml = "trash-list";
       sudo = "sudo -i";
       vdir = "vdir --color=auto";
     };
